c44128d36d705b3dd7a10ecde3441bd7188d6bcb
Paul Kelly <paul.kelly20@gmail.com>
2018-06-07 18:04:22 +0100

    Test for hero having a name passes

---
+++ Hero.js
@@ -0,0 +1,10 @@
+var Hero = function(name, favouriteFood, tasks){
+    this.name = name;
+    this.favouriteFood = favouriteFood;
+    this.tasks = tasks;
+    this.health = 100;
+  };
+
+
+
+module.exports = Hero;
---
+++ specs/Hero_spec.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+var Task = require('../Task.js');
+var Food = require('../Food.js');
+var Hero = require('../Hero.js');
+
+var food;
+var food2;
+var task;
+var task2;
+var task3;
+var hero;
+
+beforeEach(function(){
+  food = new Food("Steak", 20);
+  food2 = new Food("Chicken", 10);
+  task = new Task(20, 100, "Car");
+  task2 = new Task(30, 5, "House");
+  task3 = new Task(40, 40, "Speedboat");
+  taskCollection = [task, task2, task3];
+  hero = new Hero("Paul", "Chicken",taskCollection )
+});
+
+it("Hero has a name", function(){
+  assert.strictEqual(hero.name, "Paul");
+});
